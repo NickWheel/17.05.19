1ee3dd388a12349c5f062c210a0aa257698b814f
Nick <sungamma.99@gmail.com>
2019-05-17 00:27:13 +0300

    done dz!

---
+++ main.js
@@ -0,0 +1,25 @@
+let a = 0;
+let b = a ** 2;
+
+if (a < 1337) {
+    console.log('HAHA, small gaayyyyyyyyyyy');
+}
+else if (a > 1337) {
+    console.log('haha, BIG gaayyyyyyy');
+}
+else if (a == 1337) {
+    console.log('CONGRATULATIONS! You have won ... Nothing!!')
+} 
+else {
+    console.log('aaaa tak tu vvel ' + typeof a)
+}
+if (b > a) {
+    console.log('big boiii')
+}
+else {
+    console.log('lol or kek, sho za gavno, eto zhe stroka pahodu or ERROR')
+}
+
+
+arr = ['zhopa', 1337, true]
+console.log(arr[0] + ', ' + arr[1] + ' and ' + (arr.length - 1));
